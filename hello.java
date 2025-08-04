2537a41ec798ede6cf82ffb33cd783c44e947815
AzkaAzzamOnly <azka_mubarok_34rpl@student.smktelkom-mlg.sch.id>
2025-08-04 13:16:47 +0700

    Add files via upload

--- hello.java
+++ hello.java
@@ -1,6 +1,14 @@
 public class hello {
     
     public static void main (String[] args) {
-        System.out.println("x:Selamat Belajar;");
+        //main method
+        System.out.println("x:AzkaAzzamMubarok;");
+        System.out.println("x:SMK Telkom Malang;");
+        System.out.println("x:SMP IT BIC 2 BONDOWOSO;");
+        System.out.println("x:MohammadWisyamWildani;");
+        System.out.println("x:YuliastiniAry;");
+        System.out.println("x:Programmer;");
+        System.out.println("x:Calisthenics;");
     }
-}
+    }
+
