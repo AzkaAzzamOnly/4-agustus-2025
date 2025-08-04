095bcef2c3f37903d907844778ac2c58a41fe329
AzkaAzzamOnly <azka_mubarok_34rpl@student.smktelkom-mlg.sch.id>
2025-08-04 14:17:52 +0700

    Add files via upload

---
+++ teman.java
@@ -0,0 +1,36 @@
+public class teman {
+    public static void main(String[] args) {
+        //main method
+        String nama1 ="Azka Rullah Feeda Ul Haqq";
+        int usia1 = 15;
+        String cita1 = "Web Developer";
+        String nama2 = "Arjuna Susila Mukti";
+        int usia2 = 15;
+        String cita2 = "Programmer";
+        String nama3 = "Ghaisan Afif Al Ghifari";
+        int usia3 = 15;
+        String cita3 = "Software Engineer";
+        String nama4 = "Baihaqqi Jabbar Zainuar";
+        int usia4 = 16;
+        String cita4 = "SUkses Dunia Akhirat";
+        String nama5 = "Nabil Aryaguna Wijatmoko";
+        int usia5 = 17;
+        String cita5 = "Pembalap";
+
+        System.out.println(nama1);
+        System.out.println(usia1);
+        System.out.println(cita1);
+        System.out.println(nama2);
+        System.out.println(usia2);
+        System.out.println(cita2);
+        System.out.println(nama3);
+        System.out.println(usia3);
+        System.out.println(cita3);
+        System.out.println(nama4);
+        System.out.println(usia4);
+        System.out.println(cita4);
+        System.out.println(nama5);
+        System.out.println(usia5);
+        System.out.println(cita5);
+    }
+}
